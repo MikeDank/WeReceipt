1520b8c5f0f9800d92bf2ab004171604b8a6b642
Tibroni <adriantibroni@gmail.com>
2020-12-12 18:53:03 -0600

    Initial Commit

---
+++ WeReceipt/WeReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeReceiptApp.swift
+//  WeReceipt
+//
+//  Created by Leonard Tibrea on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WeReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
